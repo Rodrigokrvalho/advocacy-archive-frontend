27d3eff37b3438600ac6071b88a93a722444b26c
Rodrigo Carvalho <54452396+Rodrigokrvalho@users.noreply.github.com>
2023-08-18 15:41:08 -0300

    form variables on useeffect

--- src/components/forms/ProcessForm/index.tsx
+++ src/components/forms/ProcessForm/index.tsx
@@ -58,7 +58,7 @@ export function ProcessForm({
       setValue('action', formValues?.action || '');
       setValue('status', formValues?.status || '');
     }
-  }, [isEditing, initialValue, formValues]);
+  }, [isEditing, initialValue, formValues, setValue]);
 
 
   async function handleEditProcess(data: ProcessDataForm) {
